68b09f911047c512a8fdd1fe3e7adf9e6542d091
EKB1130 <102796004+EKB1130@users.noreply.github.com>
2025-03-26 17:52:54 +0000

    Create WarChest.java

---
+++ src/wars/WarChest.java
@@ -0,0 +1,50 @@
+package wars;
+
+/**
+ * Represents the Admiral's war chest holding funds for commissioning ships,
+ * decommissioning them, and handling encounter prize money adjustments.
+ */
+public class WarChest {
+    private int balance;
+
+    /**
+     * @param initialBalance the starting amount of money
+     */
+    public WarChest(int initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    /**
+     * @return the war chest balance
+     */
+    public int getBalance() {
+        return balance;
+    }
+
+    /**
+     * @param amount the amount to add
+     */
+    public void add(int amount) {
+        balance += amount;
+    }
+
+    /**
+     * Deducts the specified amount from the war chest.
+     * 
+     * Note: This method allows the balance to go negative
+     * 
+     * @param amount the amount to deduct
+     */
+    public void deduct(int amount) {
+        balance -= amount;
+    }
+
+    /**
+     * Provides a textual representation of the war chest.
+     * @return a string displaying the current balance
+     */
+    @Override
+    public String toString() {
+        return "WarChest balance: " + balance;
+    }
+}
