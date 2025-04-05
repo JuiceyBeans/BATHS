7c099ff19fdd342ccbdbe201bb6d40c12cea6ed2
Karthi Suresh <75553966+JuiceyBeans@users.noreply.github.com>
2025-04-05 14:01:27 +0100

    Ship types

---
+++ src/wars/ships/Frigate.java
@@ -0,0 +1,36 @@
+package wars.ships;
+
+import wars.api.Ship;
+/**
+ * Frigates can have cannons, unlike other ships. Frigates can also have a doctor or a pinnace
+ * @author Karthi (Ayush) Suresh
+ * @version 27/03/2025
+ */
+public class Frigate extends Ship {
+    private boolean hasPinnace = false;
+
+    public Frigate(String name, int cannons, int battleSkill, boolean hasPinnace) {
+        super(name, cannons * 10, battleSkill);
+        this.hasPinnace = hasPinnace;
+    }
+
+    @Override
+    public boolean hasPinnace() {
+        return this.hasPinnace;
+    }
+
+    @Override
+    public boolean canBlockade() {
+        return hasPinnace();
+    }
+
+    @Override
+    public boolean canBattle() {
+        return true;
+    }
+
+    @Override
+    public boolean canSkirmish() {
+        return false;
+    }
+}
---
+++ src/wars/ships/ManOWar.java
@@ -0,0 +1,61 @@
+package wars.ships;
+
+import wars.api.Ship;
+/**
+ * Man o wars have multiple decks and can hold a number of marines, unlike other ships
+ * @author Karthi (Ayush) Suresh
+ * @version 27/03/2025
+ */
+public class ManOWar extends Ship {
+    private int marines;
+    private int decks;
+
+    public ManOWar(String name, int battleSkill, int marines, int decks, int cannons) {
+        super(name, cannons > 2 ? 300 : 500, battleSkill);
+    }
+
+    /**
+     * @return Number of marines on the ship
+     */
+    public int getMarines() {
+        return this.marines;
+    }
+
+    /**
+     * Set number of marines on the ship
+     * @param marines Number of marines
+     */
+    public void setMarines(int marines) {
+        this.marines = marines;
+    }
+
+    /**
+     * @return Number of decks on the ship
+     */
+    public int getDecks() {
+        return this.decks;
+    }
+
+    /**
+     * Set number of decks on the ship
+     * @param decks Number of decks
+     */
+    public void setDecks(int decks) {
+        this.decks = decks;
+    }
+
+    @Override
+    public boolean canBlockade() {
+        return true;
+    }
+
+    @Override
+    public boolean canBattle() {
+        return true;
+    }
+
+    @Override
+    public boolean canSkirmish() {
+        return true;
+    }
+}
